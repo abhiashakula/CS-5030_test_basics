4155711823e320b58f121ae4d1ce8a97eeaeae7a
abhiashakula <113057601+abhiashakula@users.noreply.github.com>
2022-11-07 16:12:05 -0600

    Test cases done

--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -12,25 +12,40 @@ class todoservice{
             "title": "T1",
             "description": "D1",
             "done": false
-        }]
+            },
+        ]
     }
     constructor(){
         this.todos=this.todo_data;
     }
 
-    get_todos(){
+    get_todos() {
+        //console.log(this.todos);
         return this.todos;
+        
     }
 
-    add_todo(todo){
+    add_todo(todo) {
+        this.todos = this.todos.todo.concat(todo);
         // Your code here
     }
 
-    delete_todo(id){
+    delete_todo(id) {
+
+        this.todos = this.todos.filter(todo => todo.title !== id);
+        //console.log(this.todos);
         // Your code here
     }
 
-    update_todo(id, todo){
+    update_todo(id, todo) {
+        //console.log(this.todos);
+        this.todos.map(todo_1 => {
+            if (todo_1.title === id) {
+                todo_1.title = todo;
+            }
+           
+        })
+        //console.log(this.todos);
         // Your code here
     }
 }
